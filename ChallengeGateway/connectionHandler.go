9de6604a4d66cded6b2167e04c6289a8d7e3e5fc
Tuankien <tuankien0408@gmail.com>
2026-01-15 14:50:11 +0700

    auth gateway

---
+++ ChallengeGateway/connectionHandler.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+
+	"challenge-gateway/db" 
+	"github.com/redis/go-redis/v9"
+)
+
+func HandleConnection(clientConn net.Conn, rdb *redis.Client) {
+	defer clientConn.Close()
+
+	challengeTarget, err := authenticateClient(clientConn, rdb)
+	if err != nil {
+		fmt.Fprintln(clientConn, "Invalid or Expired Token. Goodbye!")
+		fmt.Printf("[-] Auth failed: %v\n", err)
+		return
+	}
+
+	fmt.Fprintf(clientConn, "Access Granted! Connecting to challenge...\n")
+	challengeConn, err := net.Dial("tcp", challengeTarget)
+	if err != nil {
+		fmt.Fprintf(clientConn, "[!] Could not connect to challenge server.\n")
+		return
+	}
+	defer challengeConn.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		io.Copy(challengeConn, clientConn)
+		done <- struct{}{}
+	}()
+
+	go func() {
+		io.Copy(clientConn, challengeConn)
+		done <- struct{}{}
+	}()
+
+	<-done
+	fmt.Printf("[+] Session ended: %s\n", clientConn.RemoteAddr())
+}
+
+
+func authenticateClient(conn net.Conn, rdb *redis.Client) (string, error) {
+	fmt.Fprint(conn, "\n--- CTF AUTHENTICATION ---\nPlease enter your token: ")
+
+	reader := bufio.NewReader(conn)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	token := strings.TrimSpace(input)
+	if token == "" {
+		return "", fmt.Errorf("empty token")
+	}
+
+	target, err := db.GetChallengeConnectionByToken(rdb, token)
+	if err != nil {
+		return "", err
+	}
+
+	return target, nil
+}
