2547e03c7529a77114b03c730e92150ef465bc27
tmtru <manhtrung2502@gmail.com>
2026-01-21 21:42:54 +0700

    Add ArgoOutbox model and migration for outbox table

--- FCTF-ManagementPlatform/CTFd/models/__init__.py
+++ FCTF-ManagementPlatform/CTFd/models/__init__.py
@@ -1256,3 +1256,12 @@ class Brackets(db.Model):
     name = db.Column(db.String(255))
     description = db.Column(db.Text)
     type = db.Column(db.String(80))
+
+
+class ArgoOutbox(db.Model):
+    __tablename__ = "argo_outbox"
+    id = db.Column(db.Integer, primary_key=True)
+    payload = db.Column(db.JSON, nullable=False)
+    status = db.Column(db.Integer, default=0)
+    expiry = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
