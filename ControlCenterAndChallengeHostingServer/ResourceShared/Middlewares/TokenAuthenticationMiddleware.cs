84e701d420036c6a00b8cb930155b7cfc06c5b5b
Do Minh Duc (K18 HL) <ducdmhe181735@fpt.edu.vn>
2026-03-10 22:26:40 +0700

    feat: Remove helmfile.yaml to streamline deployment configuration

--- ControlCenterAndChallengeHostingServer/ResourceShared/Middlewares/TokenAuthenticationMiddleware.cs
+++ ControlCenterAndChallengeHostingServer/ResourceShared/Middlewares/TokenAuthenticationMiddleware.cs
@@ -165,7 +165,6 @@ public class TokenAuthenticationMiddleware
                         // ignore cache errors
                     }
             }
-
             await _next(context);
         }
         catch (Exception ex)
