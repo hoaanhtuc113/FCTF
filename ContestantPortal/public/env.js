b187bf95548a84d74a99b30ad420500b5ca551c7
dohoanganh2004 <dohoanganh92004@gmail.com>
2026-05-25 12:29:17 +0700

    fix challange-sandbox interface, fix ticket, fix scoreboard

--- ContestantPortal/public/env.js
+++ ContestantPortal/public/env.js
@@ -1,5 +1,5 @@
 window.__ENV__ = {
-  VITE_API_URL: "http://localhost:5001",
+  VITE_API_URL: "http://localhost:5001/api",
   VITE_BASE_GATEWAY: "localhost",
   VITE_HTTP_PORT: "80",
   VITE_TCP_PORT: "9000",
