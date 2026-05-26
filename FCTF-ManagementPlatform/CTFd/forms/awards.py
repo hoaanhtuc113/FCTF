f87a062257fc76b6072dee21860204eab80833db
dohoanganh2004 <dohoanganh92004@gmail.com>
2026-05-26 13:59:58 +0700

    Fix bug validate value in award form

--- FCTF-ManagementPlatform/CTFd/forms/awards.py
+++ FCTF-ManagementPlatform/CTFd/forms/awards.py
@@ -1,5 +1,6 @@
 from wtforms import RadioField, StringField, TextAreaField
 from wtforms.fields.html5 import IntegerField
+from wtforms.validators import NumberRange
 
 from CTFd.forms import BaseForm
 from CTFd.forms.fields import SubmitField
@@ -7,7 +8,7 @@ from CTFd.forms.fields import SubmitField
 
 class AwardCreationForm(BaseForm):
     name = StringField("Name")
-    value = IntegerField("Value", default=0)
+    value = IntegerField("Value", default=0, validators=[NumberRange(min=0, message="Value must be 0 or greater")])
     category = StringField("Category")
     description = TextAreaField("Description")
     submit = SubmitField("Create")
