c346e294a2df34c1721c42fb4a0f0e6286e04b14
Do Minh Duc (K18 HL) <ducdmhe181735@fpt.edu.vn>
2026-05-27 15:06:05 +0700

    fix: remove redundant state assignment for challenge visibility on deployment success

--- FCTF-ManagementPlatform/CTFd/api/v1/challenges.py
+++ FCTF-ManagementPlatform/CTFd/api/v1/challenges.py
@@ -1271,7 +1271,6 @@ class ChallengeDeploy(Resource):
 
             if workflow_phase == "Succeeded":
                 challenge.deploy_status = "DEPLOY_SUCCESS"
-                challenge.state = "visible"
                 db.session.commit()
 
             elif workflow_phase in ("Failed", "Error"):
